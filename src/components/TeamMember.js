30f7d763534072e81b580c1949fe9fa0f62aed82
Kakuwa <taras_grytsa@yahoo.com>
2020-11-20 15:06:31 +0000

    commit2

--- src/components/TeamMember.js
+++ src/components/TeamMember.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import '../App.css';
 
+
+
 export default function TeamMember(props) {
  
   return (
     <div>
-        <h4 className = 'clear'>{props.name}</h4>
+        <h3 className = 'clear'>{props.name}</h3>
         <div>
-          <img className='team__image' src = 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/beach-quotes-1559667853.jpg' alt = '' />
-          <p className = 'team__text'>Mr Kakadoon is magnificent</p>
+          <img className='team__image' src = {props.imageUrl} alt = '' />
+          <p className = 'team__text'>{props.text1}</p>
+          <p className = 'team__text'>{props.text2}</p>
         </div>
 
 
